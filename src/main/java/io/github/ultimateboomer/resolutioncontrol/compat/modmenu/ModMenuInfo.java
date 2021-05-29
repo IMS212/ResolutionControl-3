f4c96e8fe566595ef916ed9d5064973148686956
Steven Cao <caohc2017@gmail.com>
2021-05-29 19:22:38 -0400

    Use newer mod menu api

--- src/main/java/io/github/ultimateboomer/resolutioncontrol/compat/modmenu/ModMenuInfo.java
+++ src/main/java/io/github/ultimateboomer/resolutioncontrol/compat/modmenu/ModMenuInfo.java
@@ -1,20 +1,12 @@
 package io.github.ultimateboomer.resolutioncontrol.compat.modmenu;
 
-import io.github.prospector.modmenu.api.ModMenuApi;
-import net.minecraft.client.gui.screen.Screen;
-import io.github.ultimateboomer.resolutioncontrol.ResolutionControlMod;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import io.github.ultimateboomer.resolutioncontrol.client.gui.screen.MainSettingsScreen;
-
-import java.util.function.Function;
 
 public final class ModMenuInfo implements ModMenuApi {
 	@Override
-	public String getModId() {
-		return ResolutionControlMod.MOD_ID;
-	}
-	
-	@Override
-	public Function<Screen, ? extends Screen> getConfigScreenFactory() {
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
 		return MainSettingsScreen::new;
 	}
 }
